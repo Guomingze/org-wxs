cec713a7daf692e932b418eb7e303c06f6d704c1
moxiaoze <981205912@qq.com>
2018-10-31 19:38:55 +0800

    123

--- wxs-entrance/src/main/java/cn/business/Login.java
+++ wxs-entrance/src/main/java/cn/business/Login.java
@@ -1,6 +1,7 @@
 package cn.business;
 
 import cn.util.OpenIdBycode;
+import cn.util.RandomStr;
 import cn.wxs.dao.UserDao;
 import cn.wxs.pojo.User;
 import com.alibaba.fastjson.JSON;
@@ -29,9 +30,22 @@ public class Login {
         user.setUpwd(openid);
         List<User> users = userDao.allUser(user);
         if(users != null){
-            return JSON.toJSONString(users);
+            User user1 = users.get(0);
+            user1.setUpwd(null);
+            user1.setUphone(null);
+            return JSON.toJSONString(user1);
         }else{
-            return "登录失败";
+            User user1 = new User();
+            user1.setUpwd(openid);
+            user1.setUname(RandomStr.getRandomString(5));
+            int count = userDao.addUser(user1);
+            if(count < 0){
+                return "登录失败，请重试";
+            }
+            user1.setUpwd(openid);
+            user1 = userDao.allUser(user1).get(0);
+            user1.setUpwd(null);
+            return JSON.toJSONString(user1);
         }
     }
 }
