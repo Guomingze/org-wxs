527bdf526fccabf3f0f5f4f2413ddb415c84bf0b
MLIY <1927026348@qq.com>
2018-10-31 11:37:51 +0800

    MLIY

--- wxs-dao/src/main/java/cn/wxs/dao/DishesDao.java
+++ wxs-dao/src/main/java/cn/wxs/dao/DishesDao.java
@@ -1,5 +1,28 @@
 package cn.wxs.dao;
 
-public interface DishesDao {
+import cn.wxs.pojo.Dishes;
 
+import java.util.List;
+
+public interface DishesDao {
+    /**
+     * 查找所有菜品种类
+     * @param dishes 菜品种类对象
+     * @return  菜品种类集合
+     */
+    List<Dishes> getAllDishes(Dishes dishes);
+
+    /**
+     *  添加一个菜品种类
+     * @param dishes  菜品种类对象
+     * @return  受影响行数
+     */
+    int addDishes(Dishes dishes);
+
+    /**
+     * 修改菜品种类
+     * @param dishes 菜品种类对象
+     * @return  受影响行数
+     */
+    int updateDishes(Dishes dishes);
 }
